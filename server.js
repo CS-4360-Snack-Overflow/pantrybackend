d15e6a8b2d3cc5fa0b78c97135b33da82c864c88
Wolfy319 <jonwolffiorini@gmail.com>
2023-04-24 14:30:53 -0600

    http

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ const store = MongoStore.create({
 app.use(session({
   secret: 'my-secret',
   resave: false,
-  secure: false,
+  httpOnly: false,
   saveUninitialized: false,
   store: store,
 }));
