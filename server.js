19e70ea209af2f26a2a6af0062f2c37183f9739c
Wolfy319 <jonwolffiorini@gmail.com>
2023-04-24 16:31:55 -0600

    asldfkj

--- server.js
+++ server.js
@@ -64,7 +64,6 @@ const store = MongoStore.create({
 app.use(session({
   secret: 'my-secret',
   resave: false,
-  cookie: {  httpOnly: false, secure: true, sameSite: "none" },
   saveUninitialized: false,
   store: store,
 }));
