cdee90c74ad289e7ddac2be46422cb59e32f40ca
Wolfy319 <jonwolffiorini@gmail.com>
2023-04-29 14:40:54 -0600

    maybe this will work?

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -160,14 +160,16 @@ router.get('/logout', requireAuth, async (req, res) => {
 
 // Add a recipe ID to user's favorites
 router.post('/favorite/:id', async (req, res) => {
-  await User.findOneAndUpdate({_id: req.session.userId}, {$addToSet: {favoriteRecipes: req.params.id}}, 
+  const user = await User.findOneAndUpdate({_id: req.session.userId}, {$addToSet: {favoriteRecipes: req.params.id}}, 
                         (err) => {console.log(err)});
+  return user.favoriteRecipes
 })
 
 // Remove a recipe ID from user's favorites
 router.delete('/unfavorite/:id', async (req, res) => {
-  await User.findOneAndUpdate({_id: req.session.userId}, {$pull: {favoriteRecipes: req.params.id}}, 
+  const user = await User.findOneAndUpdate({_id: req.session.userId}, {$pull: {favoriteRecipes: req.params.id}}, 
                         (err) => {console.log(err)})
+  return user.favoriteRecipes
 })
 
 router.get('/isfavorite/:id', async (req, res) => {
