01dd70b38cd566f85798266e0376158dd9c607f9
Wolfy319 <jonwolffiorini@gmail.com>
2023-04-29 14:13:31 -0600

    mess with cors

--- server.js
+++ server.js
@@ -34,8 +34,7 @@ const cors=require("cors");
 const corsOptions ={
    origin:'https://pantrydev.netlify.app', 
    credentials:true,       //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-   methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"]
+   optionSuccessStatus:200
 }
 
 // Express app instance
